39d9e38d63dfca4f7645cc185eb8efff59edd49e
pibo0121040 <pibo0121040@CL329-03>
2023-09-16 17:10:23 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите a ");
+            int a= Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите b ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int v= a*a*a;
+            int s = 6 * (a * a);
+            Console.Write($"v: {v}  s: {s}");
+        }
+    }
+
+}
